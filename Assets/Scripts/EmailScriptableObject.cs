f22e82b0e571fc845d65653df58329d1cd3ecfd5
Reece <reecebellchambers98@gmail.com>
2024-05-20 16:16:14 +0100

    Implemented scriptable object that works with the email menu system
    Need to work out to switch different scriptable objects during runtime.

---
+++ Assets/Scripts/EmailScriptableObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EmailScriptableObject", menuName = "ScriptableObjects/EmailScriptableObject")]
+public class EmailScriptableObject : ScriptableObject
+{
+    public string target = null;
+    public string floor = null;
+    public string prank = null;
+}
