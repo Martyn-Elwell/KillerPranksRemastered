8a26eb5e43de94f063684dc89daca058f36cd72f
Reece <reecebellchambers98@gmail.com>
2024-05-21 11:51:59 +0100

    Can switch between different scriptable objects using arrows.
    More data can be added later on.

--- Assets/Scripts/EmailManager.cs
+++ Assets/Scripts/EmailManager.cs
@@ -1,25 +1,59 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class EmailManager : MonoBehaviour
 {
-    [SerializeField] private EmailScriptableObject emailScriptableObject;
+    [SerializeField] private List<EmailScriptableObject> emailScriptableObject;
 
     [SerializeField] private Text targetNameText;
     [SerializeField] private Text floorNameText;
     [SerializeField] private Text prankNameText;
 
+    [SerializeField] private int num;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        targetNameText.text = emailScriptableObject.target;
-        floorNameText.text = emailScriptableObject.floor;
-        prankNameText.text = emailScriptableObject.prank;
+        targetNameText.text = emailScriptableObject[0].target;
+        floorNameText.text = emailScriptableObject[0].floor;
+        prankNameText.text = emailScriptableObject[0].prank;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        switch(num)
+        {
+            case -1:
+                num++;
+                break;
+            case 0:
+                targetNameText.text = emailScriptableObject[0].target;
+                floorNameText.text = emailScriptableObject[0].floor;
+                prankNameText.text = emailScriptableObject[0].prank;
+                break;
+            case 1:
+                targetNameText.text = emailScriptableObject[1].target;
+                floorNameText.text = emailScriptableObject[1].floor;
+                prankNameText.text = emailScriptableObject[1].prank;
+                break;
+            default:
+                Debug.Log("Unknown Data");
+                break;
+
+        }
+    }
+
+    public void ForwardArrow()
+    {
+        Debug.Log("Forward arrow pressed");
+        num++;
+    }
+
+    public void BackwardArrow()
+    {
+        Debug.Log("Back arrow pressed");
+        num--;
     }
 }
