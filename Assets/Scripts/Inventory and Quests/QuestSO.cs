ff88e957de6c39cc74315b2ac017033785aaf8c6
Zac <zachery2.pavey@live.uwe.ac.uk>
2024-05-29 15:04:49 +0100

    Started Quests Setup
    Created SO template for quests, needs further datapoints added into it.

---
+++ Assets/Scripts/Inventory and Quests/QuestSO.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "QuestData", menuName = "ScriptableObjects/QuestData")]
+public class QuestSO : ScriptableObject
+{
+    public int questID;
+    public string questDescription;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
