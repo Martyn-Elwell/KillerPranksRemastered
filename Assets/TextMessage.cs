50d25d77a102b3b8096b06ae5300465fc2a68b48
Josh <joshua2.sorley@live.uwe.ac.uk>
2024-06-03 07:26:16 +0100

    Texts Hurt my Head

--- Assets/TextMessage.cs
+++ Assets/TextMessage.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TextMessage : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public string Message;
+    public TMP_Text Text;
     void Start()
     {
-        
+        Text.SetText(Message.ToString());
     }
 
     // Update is called once per frame
